43109040735855237aebfb917c08c227eedcc611
红色枫叶 <BlackMaple@live.cn>
2025-01-01 19:21:14 +0800

    1

--- Maple.MonoGameAssistant.MetadataDemo/Program.cs
+++ Maple.MonoGameAssistant.MetadataDemo/Program.cs
@@ -2,7 +2,13 @@
 {
     public static partial class Program
     {
-        static void Main() { }
+        static void Main() {
+
+            List<int> a = [1, 2, 3];
+            var t = a.Find(p=>p==1);
+            var tt = a.FirstOrDefault(p => p == 1);
+            
+        }
     }
 }
 
