206c7db069b6ffab6cb28f2264cee034ae9d61fc
红色枫叶 <BlackMaple@live.cn>
2025-04-24 19:48:58 +0800

    1.fixed ui

--- Maple.MonoGameAssistant.GameSSR/Program.cs
+++ Maple.MonoGameAssistant.GameSSR/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddMasaBlazor(p =>
     });
 });
 
-builder.Services.AddHttpClient<GameHttpClientService>(p => p.BaseAddress = new Uri("http://localhost:61114/"))
+builder.Services.AddHttpClient<GameHttpClientService>(p => p.BaseAddress = new Uri("http://localhost:11964/"))
     .ConfigurePrimaryHttpMessageHandler(p => new HttpClientHandler() { AutomaticDecompression = System.Net.DecompressionMethods.Brotli });
 
 
